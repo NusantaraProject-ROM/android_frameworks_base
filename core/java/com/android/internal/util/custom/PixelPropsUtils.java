cf073b1d9253c5d3b69f8d4c6632efa61d664d73
Sa-Sajjad <111744107+Sa-Sajjad@users.noreply.github.com>
2023-01-15 07:23:06 +0600

    Update PixelPropsUtils.java

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -149,7 +149,7 @@ public class PixelPropsUtils {
         propsToChangePixel7Pro.put("DEVICE", "cheetah");
         propsToChangePixel7Pro.put("PRODUCT", "cheetah");
         propsToChangePixel7Pro.put("MODEL", "Pixel 7 Pro");
-        propsToChangePixel7Pro.put("FINGERPRINT", "google/cheetah/cheetah:13/TQ1A.221205.011/9244662:user/release-keys");
+        propsToChangePixel7Pro.put("FINGERPRINT", "google/cheetah/cheetah:13/TQ1A.230105.002/9325679:user/release-keys");
         propsToChangePixel5 = new HashMap<>();
         propsToChangePixel5.put("BRAND", "google");
         propsToChangePixel5.put("MANUFACTURER", "Google");
